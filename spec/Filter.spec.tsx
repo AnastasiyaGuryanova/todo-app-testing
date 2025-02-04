b626a3053e7eaa3ef5fe3718919a65b6d677058a
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-02-04 19:31:02 +0300

    chore: первичная настройка проекта и добавление шаблонов тестов

---
+++ spec/Filter.spec.tsx
@@ -0,0 +1,5 @@
+import "@testing-library/jest-dom";
+
+describe("Кнопка фильтрации", () => {
+  it.todo("Кнопка фильтрации");
+});
---
+++ spec/index.spec.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom";
+//import { render } from "@testing-library/react";
+//import { App } from "../src/App";
+
+describe("Планирование тестов", () => {
+  it.todo("Добавить новые задачи");
+  it.todo("Просматривать список всех задач");
+  it.todo("Отмечать задачи как выполненные");
+  it.todo("Удалять задачи");
+  it.todo("Фильстровать задачи по статусу");
+});
