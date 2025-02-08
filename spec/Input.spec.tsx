d09420e3e7ea517708d574fd497f2cc6742449b4
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-02-08 17:03:05 +0300

    feat: добавление компонента Input и тестов для него

--- spec/Input.spec.tsx
+++ spec/Input.spec.tsx
@@ -1,6 +1,19 @@
 import "@testing-library/jest-dom";
+import { prettyDOM, render } from "@testing-library/react";
+import { Input } from "../src/components/Input";
 
 describe("Поле ввода", () => {
   it.todo("Ограничение на ввод более 32 символов");
-  it.todo("Поле доступно для ввода");
+  it("Поле доступно для ввода", () => {
+    const fn = jest.fn();
+    const span = document.createElement("span");
+
+    const { container } = render(<Input value="Greetings" onChange={fn} />, {
+      container: document.body.appendChild(span),
+      hydrate: false,
+      legacyRoot: false,
+    });
+
+    console.log(prettyDOM(container));
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { useState } from "react";
+import { Input } from "./components/Input";
+
 export const App = () => {
-  return <div>Testing</div>;
+  const [inputValue, setinputValue] = useState("");
+
+  return (
+    <div>
+      <Input value={inputValue} onChange={(val) => setinputValue(val)} />
+    </div>
+  );
 };
