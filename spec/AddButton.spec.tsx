26ad93784c351376067a2175c6f78973b7b6e8f6
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-02-12 09:49:57 +0300

    feat: добавление компонента AddButton и тестов для него

--- spec/AddButton.spec.tsx
+++ spec/AddButton.spec.tsx
@@ -1,6 +1,24 @@
 import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { AddButton } from "../src/components/AddButton";
 
-describe("Кнопка добавить", () => {
-  it.todo("Должна блокироваться для строки больше 32 символов");
-  it.todo("Должна блокироваться для строки меньше 1 символа");
+import uE from "@testing-library/user-event";
+
+describe('Кнопка "Добавить"', () => {
+  const userEvent = uE.setup();
+
+  it.todo("Блокировка для строки больше 32 символов");
+  it.todo("Блокировка для строки меньше 1 символа");
+
+  it("Проверка кликов по кнопке", async () => {
+    const fn = jest.fn();
+
+    render(<AddButton onClick={fn} />);
+
+    const button = screen.getByText(/добавить/i);
+
+    await userEvent.click(button);
+
+    expect(fn).toBeCalledTimes(1);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import { Input } from "./components/Input";
+import { AddButton } from "./components/AddButton";
+import "./styles.css";
 
 export const App = () => {
   const [inputValue, setinputValue] = useState("");
 
+  const handleAddButtonClick = () => {
+    console.log("Button was clicked!");
+  };
+
   return (
-    <div>
-      <Input value={inputValue} onChange={(val) => setinputValue(val)} />
+    <div className="root-container">
+      <Input defaultValue={inputValue} onChange={(val) => setinputValue(val)} />
+      <AddButton onClick={handleAddButtonClick} />
     </div>
   );
 };
