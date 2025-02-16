5b9eec19f3bd20cb71bb51ab261ebabba18ad132
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-02-16 17:45:15 +0300

    refactor: правки в компоненте Input

--- spec/Input.spec.tsx
+++ spec/Input.spec.tsx
@@ -2,13 +2,23 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { Input } from "../src/components/Input";
 
-import uE from "@testing-library/user-event";
+import ue from "@testing-library/user-event";
 
 describe("Поле ввода", () => {
-  const userEvent = uE.setup({ delay: 100 });
+  const userEvent = ue.setup({
+    advanceTimers: jest.advanceTimersByTime,
+  });
 
-  it("Ограничение на ввод более 32 символов", () => {
-    expect("Greetings!").toBeShorterThan(32);
+  it("Ограничение на ввод более 32 символов", async () => {
+    const fn = jest.fn();
+    render(<Input defaultValue="Hi!" onChange={fn} />);
+
+    const hintEl = screen.getByTestId("input-hint-text");
+    const inputEl = screen.getByRole("textbox");
+    await userEvent.clear(inputEl);
+    await userEvent.type(inputEl, "Пример заголовка");
+
+    expect(hintEl.innerHTML).toBe("");
   });
 
   it("Поле доступно для ввода", async () => {
